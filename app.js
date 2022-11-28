b49528f43e5d5e5c38e920f7bd8bc475e92418c6
marjohncornelio <112856637+marjohncornelio@users.noreply.github.com>
2022-11-29 00:18:02 +0800

    node-cron

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require("express");
 const app = express();
 const cron = require("node-cron");
 const axios = require("axios");
+const cors = require("cors");
 
+app.use(cors());
 app.post("/recurring-rent", (req, res) => {
-  const fetchData = async () => {
-    await axios
-      .post("https://surewin-backend.onrender.com/recurring-rent")
-      .then((res) => {
-        res.status(200).json({ success: true, mgs: "success" });
-      })
-      .catch((error) => {
-        res.status(400).json({ success: false, mgs: "something  went wrong" });
-      });
-  };
-  fetchData();
+  cron.schedule("0 0 0 * * *", () => {
+    const fetchData = async () => {
+      await axios
+        .post("https://surewin-backend.onrender.com/recurring-rent")
+        .then((res) => {
+          res.status(200).json({ success: true, mgs: "success" });
+        })
+        .catch((error) => {
+          res.status(400).json({ success: false, mgs: "something  went" });
+        });
+    };
+    fetchData();
+  });
 });
 app.get("/", (req, res) => {
   res.status(200).json({ success: true, msg: "Success" });
